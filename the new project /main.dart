c5fe99cd80975ec79d980fe2f80899ff7d4683ce
malak-angel222 <hadjilamalekk@gmail.com>
2025-05-04 19:48:29 +0100

    Create main.dart

---
+++ the new project /main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'firestore_service.dart';
+import 'auth_screens.dart';
+import 'main_app.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseService().initialize();
+  runApp(StudyFlowApp());
+}
+
+class StudyFlowApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Study Flow',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignUpPage(),
+        '/student-signup': (context) => StudentSignUpPage(),
+        '/company-signup': (context) => CompanySignUpPage(),
+        '/forgot-password': (context) => ForgotPasswordPage(),
+        '/reset-password': (context) => ResetPasswordPage(),
+        '/home': (context) => StudyFlowHome(),
+      },
+    );
+  }
+}
